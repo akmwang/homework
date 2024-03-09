4ecc573ab561f2029bd874d8eaab4328572d14c3
wangHuayi <1498709237@qq.com>
2024-03-09 10:53:58 +0800

    Calculator v1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Calcu
+{
+    public static void Calculate(int a, int b, char c)
+    {
+        switch(c)
+        {
+            case '+':
+                Console.WriteLine(a + b);
+                break;
+            case '-':
+                Console.WriteLine(a - b);
+                break;
+            case '*':
+                Console.WriteLine(a * b);
+                break;
+            case '/':
+                Console.WriteLine(a / b);
+                break;
+            default: 
+                Console.WriteLine("Wrong");
+                break;
+        }
+    }
+    static void Main()
+    {
+        Calcu.Calculate(1, 2, '+');
+        Calcu.Calculate(1, 2, '-');
+        Calcu.Calculate(1, 2, '*');
+        Calcu.Calculate(1, 2, '/');
+    }
+}
